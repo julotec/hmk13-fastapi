6c2c5ac6a547a390a15e0f77848fd2d6471b38fa
= <135429630+julotec@users.noreply.github.com>
2024-02-24 23:09:52 +0100

    first commit

---
+++ src/database/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Boolean, func
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('created_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+    confirmed = Column(Boolean, default=False)
